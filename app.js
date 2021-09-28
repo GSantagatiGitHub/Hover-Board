5529becbc20d5270f3a4afbc94cb07f1fb5c8b24
GSantagati <gs@414customweb.com>
2021-09-28 11:37:12 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const container = document.getElementById('container');
+const colors = ['red', 'yellow', 'orange', 'lightgreen', 'lightblue', 'purple'];
+const squares = 2000;
+
+for (let i = 0; i < squares; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+square.addEventListener('mouseover', () => setColor(square));
+
+square.addEventListener('mouseout', () => removeColor(square));
+
+container.appendChild(square);
+
+}
+
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.background = color;
+    element.style.boxShadow = `0 0 3px ${color}, 0 0 8px ${color}`;
+}
+
+function removeColor(element) {
+    element.style.background = '#1d1d1d';
+    element.style.boxShadow = '0 0 3px';
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
